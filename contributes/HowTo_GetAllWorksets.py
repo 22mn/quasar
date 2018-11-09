82aec84f16a5f0adfd1cef1bac02553ddd50478e
mgj <p.jean90@gmail.com>
2018-11-09 11:11:44 +0800

    news

---
+++ contributes/HowTo_GetAllWorksets.py
@@ -0,0 +1,24 @@
+"""
+GET ALL WORKSETS FROM THE CURRENT DOCUMENT
+
+TESTED REVIT API: 2016,2017,2018
+
+Author: min.naung@https://twentytwo.space/contact | https://github.com/mgjean
+
+This file is shared on www.revitapidocs.com
+For more information visit http://github.com/gtalarico/revitapidocs
+License: http://github.com/gtalarico/revitapidocs/blob/master/LICENSE.md
+"""
+from Autodesk.Revit.DB import FilteredWorksetCollector, WorksetKind
+
+# document instance
+doc = __revit__.ActiveUIDocument.Document
+
+
+# collect user created worksets
+worksets = FilteredWorksetCollector(doc).OfKind(WorksetKind.UserWorkset).ToWorksets()
+
+
+# print name, workset
+for workset in worksets:
+	print workset.Name,workset
