35331f05c2debc1cac5dcb88e9d83ddd4b47fc51
mgjean <p.jean90@gmail.com>
2019-03-31 13:17:43 +0800

    node icons

---
+++ Contribution/template.py
@@ -0,0 +1,11 @@
+import clr
+clr.AddReference("RevitAPI")
+clr.AddReference("RevitServices")
+
+from Autodesk.Revit.DB import *
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+doc = DocumentManager.Instance.CurrentDBDocument
+
+TransactionManager.Instance.EnsureInTransaction()
